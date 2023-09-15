7ad3b84550989bc66a8505fa8f36354dc0a55136
Anirudh Dasari <dasarianirudh09@gmail.com>
2023-09-15 12:59:29 +0530

    process

--- Streams/ad-conversions - lambda.py
+++ Streams/ad-conversions - lambda.py
@@ -1,35 +1,30 @@
 import boto3
 import base64
 import json
+from decimal import Decimal
 
 def lambda_handler(event, context):
 
     records = event.get("Records")
-    dynamodb = boto3.client('dynamodb')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("ad-click-conversion")
 
     for record in records:
 
         payload=base64.b64decode(record["kinesis"]["data"])
         dict = json.loads(payload)
-        ad_id = str(dict['ad_id'])
+        ad_id = dict['ad_id']
         user_id = dict['user_id']
+        dict['revenue'] = Decimal(dict['revenue'])
 
-        clickItem = dynamodb.get_item(TableName='ad-click-conversion', Key={'user_id':{'S':user_id},'ad_id':{'N':ad_id}})
-        dict['conversion_timestamp'] = clickItem["Item"]["conversion_timestamp"]["S"]
-        dict['product_id'] = clickItem["Item"]["product_id"]["S"]
-        dict['revenue'] = str(clickItem["Item"]["revenue"]["N"])
-
-        response = dynamodb.put_item(TableName='ad-click-conversion', 
-                        Item={'ad_id':{'N':ad_id},
-                                'user_id':{'S':user_id},
-                                'gender':{'S':dict['gender']},
-                                'interests':{'S':dict['interests']},
-                                'age':{'N':dict['age']},
-                                'campaign':{'S':dict['campaign']},
-                                'product':{'S':dict['product']},
-                                'target_end_date':{'S':dict['target_end_date']},
-                                'target_start_date':{'S':dict['target_start_date']},
-                                'revenue':{'N':dict['revenue']},
-                                'product_id':{'S':dict['product_id']},
-                                'conversion_timestamp':{'S':dict['conversion_timestamp']},
-                        })
+        clickItem = table.get_item(Key={'ad_id': ad_id, 'user_id': user_id})
+        dict['gender'] = clickItem["Item"]["gender"]
+        dict['interests'] = clickItem["Item"]["interests"]
+        dict['age'] = int(clickItem["Item"]["age"])
+        dict['campaign'] = clickItem["Item"]["campaign"]
+        dict['product'] = clickItem["Item"]["product"]
+        dict['target_end_date'] = clickItem["Item"]["target_end_date"]
+        dict['target_start_date'] = clickItem["Item"]["target_start_date"]
+        print(dict)
+        response = table.put_item( Item=dict )
+        print(response)
